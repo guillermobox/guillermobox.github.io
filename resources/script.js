88972663988e002ab2b8dd6f7bd164796f57f69e
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2015-02-03 03:54:59 +0100

    Small cosmetic changes

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,5 @@
+---
+---
 
 function render_github_error(data, status, xhr) {
   $("#git-repos").append("<div class=\"panel-body\">Error connecting to github API</div>");
@@ -7,20 +9,21 @@ function render_github_repos(data, status, xhr){
   var div = $("#git-repos");
   var template = $("#template-github").html();
   var regexp = /git:\/\/github\.com\/([^\/]+)\/([^\/]+)/;
+  var regmap = "ssh://git@github.com:/$1/$2/";
   function compare_by_time(e1, e2) {
     return (e1.updated_at < e2.updated_at) ? 1 : -1;
   }
 
   $.map(data, function (elem, index) {
     if (elem.fork === true) return null;
-    elem.git_url = elem.git_url.replace(regexp, "ssh://git@github.com:/$1/$2");
+    elem.git_url = elem.git_url.replace(regexp, regmap);
     return elem;
   });
 
   data.sort(compare_by_time);
   div.append(Mustache.render(template, {"repos":data}));
 
-  $("li.github_repo").hover(
+  $("div>ul>li").hover(
       function (ev) { $("div.github_links", ev.delegateTarget).show(); },
       function (ev) { $("div.github_links", ev.delegateTarget).hide(); }
       );
@@ -30,7 +33,7 @@ $(document).ready( function () {
   Mustache.tags = ["[[", "]]"];
   $.ajax({
     dataType: "json",
-    url: "https://api.github.com/users/guillermobox/repos",
+    url: "https://api.github.com/users/{{site.author.github}}/repos",
     headers: {
       Accept: "application/vnd.github.v3+json",
     },
